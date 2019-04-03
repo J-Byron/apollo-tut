e6695948532cf2b5792268b5925b903d55e19f71
Josh byron <josh@Joshs-MacBook-Air.local>
2019-04-03 16:29:03 -0500

    Adds user login, authorization, and authentication

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,27 +1,40 @@
 import React, { Component } from 'react'
 import Link from './Link.jsx'
 
-export default class LinkList extends Component {
-    render() {
-      const linksToRender = [
-        {
-          id: '1',
-          description: 'Prisma turns your database into a GraphQL API 😎',
-          url: 'https://www.prismagraphql.com',
-        },
-        {
-          id: '2',
-          description: 'The best GraphQL client',
-          url: 'https://www.apollographql.com/docs/react/',
-        },
-      ]
-  
-      return (
-        // <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
-        <div>
-            <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
-        </div>
-      )
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
     }
   }
+`
+
+export default class LinkList extends Component {
+  render() {
+    return (
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>
+          if (error) return <div>Error</div>
+    
+          const linksToRender = data.feed.links
+          console.log(data)
+          return (
+            <div>
+              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+            </div>
+          )
+        }}
+      </Query>
+    )
+  }
+}
   
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'
+import { BrowserRouter } from 'react-router-dom'
+import { AUTH_TOKEN } from './constants'
 
 // *---------* Apollo 🚀 *---------*
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'   
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
 
 /* 
     Here you create the httpLink that will connect your ApolloClient 
@@ -20,13 +23,25 @@ const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 })
 
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN)
+    console.log('Authorization Linked!');
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    }
+  })
+
 /* 
     Now you instantiate ApolloClient by passing 
     in the httpLink and a new instance of an InMemoryCache
 */
 
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 })
 
@@ -37,10 +52,13 @@ const client = new ApolloClient({
 */
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
-document.getElementById('root'));
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </BrowserRouter>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
